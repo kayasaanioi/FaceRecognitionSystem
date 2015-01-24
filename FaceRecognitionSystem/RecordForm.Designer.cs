51da6b421cf8c573e00a0b1504ad246917558173
kayasaanioi <kayasaanioi@hotmail.com>
2015-01-24 17:42:29 +0800

    design

--- FaceRecognitionSystem/RecordForm.Designer.cs
+++ FaceRecognitionSystem/RecordForm.Designer.cs
@@ -40,6 +40,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 283);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(860, 227);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
